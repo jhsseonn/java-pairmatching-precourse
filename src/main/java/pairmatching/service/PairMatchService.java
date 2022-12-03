bc29f3f779399530973f4a873984946f256cc88d
jhsseonn <hosu0125@gmail.com>
2022-12-03 17:14:51 +0900

    feat: (PairMatchService) 파일에서 명단 가져와 순서 섞어 리스트로 저장하는 기능 추가

--- src/main/java/pairmatching/service/PairMatchService.java
+++ src/main/java/pairmatching/service/PairMatchService.java
@@ -11,6 +11,25 @@ import java.util.List;
 
 public class PairMatchService {
 
+    public List<String> backCrewList = new ArrayList<>();
+    public List<String> frontCrewList = new ArrayList<>();
+    public List<String> crewList = new ArrayList<>();
+
+    public List<String> getCrewList(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+        String str;
+        while((str= reader.readLine())!=null) {
+            crewList.add(str);
+        }
+
+        reader.close();
+
+        setRandomCrewList(crewList);
+
+        return crewList;
+    }
+
     public void setRandomCrewList(List<String> crew) {
         Randoms.shuffle(crew);
     }
