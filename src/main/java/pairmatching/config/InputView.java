378397651b7d743ff452edd130e9b862bec47312
jhsseonn <hosu0125@gmail.com>
2022-12-03 17:51:59 +0900

    feat: (InputView) 입력 구문 함수 추가

---
+++ src/main/java/pairmatching/config/InputView.java
@@ -0,0 +1,22 @@
+package pairmatching.config;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    OutputView outputView = new OutputView();
+
+    public String getPairMatchFeat() {
+        outputView.printStartMatch();
+        return Console.readLine();
+    }
+
+    public String getPairMenu() {
+        outputView.printSelectMatch();
+        return Console.readLine();
+    }
+
+    public String getRematch() {
+        outputView.printExistMatchHistory();
+        return Console.readLine();
+    }
+}
