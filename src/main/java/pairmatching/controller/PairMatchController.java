acb74fa0fa910d442e1b3906d45f71f193de0cd0
jhsseonn <hosu0125@gmail.com>
2022-12-04 23:47:19 +0900

    feat: (PairMatchController) 기능별 함수 생성

---
+++ src/main/java/pairmatching/controller/PairMatchController.java
@@ -0,0 +1,52 @@
+package pairmatching.controller;
+
+import pairmatching.config.InputView;
+import pairmatching.config.StringParams;
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.domain.PairMatching;
+import pairmatching.service.PairMatchService;
+
+import java.util.List;
+
+public class PairMatchController extends StringParams {
+    public Course course;
+    public Level level;
+    public PairMatching pairMatching;
+
+    public List<String> crewList;
+
+    PairMatchService pairMatchService = new PairMatchService();
+
+    public void matchingFeature(String matchFeat) {
+        if (matchFeat.equals("1")) {
+            pairMatchCreate();
+        }
+        if (matchFeat.equals("2")) {
+            pairMatchRead();
+        }
+        if (matchFeat.equals("3")) {
+            pairMatchReset();
+        }
+        if (matchFeat.equals("Q")) {
+            exitPairMatch();
+        }
+    }
+
+    public void pairMatchCreate() {
+
+    }
+
+    public void pairMatchRead() {
+
+    }
+
+    public void pairMatchReset() {
+
+    }
+
+    public void exitPairMatch() {
+
+    }
+
+}
